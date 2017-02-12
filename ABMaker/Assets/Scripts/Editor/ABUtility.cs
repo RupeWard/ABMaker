7bf6a176e67fbdaa04933c6823cc07a7960a49cc
RupeWard <rupe@rupertward.co.uk>
2017-02-12 10:53:45 +0000

    Initial import with first stage01 asset bundle

---
+++ ABMaker/Assets/Scripts/Editor/ABUtility.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ABUtility
+{
+	[MenuItem( "ABTest/Build ALL" )]
+	static void BuildAll( )
+	{
+		BuildPipeline.BuildAssetBundles(
+			"Assets/AssetBundles",
+			BuildAssetBundleOptions.None,
+			BuildTarget.StandaloneWindows64
+			);
+	}
+}
